b64b7fd7d47dde2334f6456380cd0ccf54ee4c5d
enomotorene <enomoto.rene@gmail.com>
2019-09-04 18:35:12 +0900

    new files commit

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -2,9 +2,27 @@ import Vue from 'vue'
 import App from './App.vue'
 import './assets/css/styles.css'
 import './assets/css/bootstrap.css'
+// import router from './router';
+//import store from './store';
+import firebase from 'firebase';
 
 Vue.config.productionTip = false
 
+const config = {
+apiKey: "AIzaSyCDGWDAx3DxHR_xwHvGgNT7eh1btHDYPxA",
+authDomain: "portfolio-27b4f.firebaseapp.com",
+databaseURL: "https://portfolio-27b4f.firebaseio.com",
+projectId: "portfolio-27b4f",
+storageBucket: "",
+messagingSenderId: "310608396437",
+appId: "1:310608396437:web:a33d25b3ace4a0d3"
+}
+
+firebase.initializeApp(config);
+
+export const db = firebase.firestore();
+
 new Vue({
+  //store,
   render: h => h(App),
 }).$mount('#app')
